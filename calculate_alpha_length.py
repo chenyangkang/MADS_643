ef0b57d120efa10c2aaeda2c12dfa0b4ec6f73ec
chenyangkang <chenyangkang24@outlook.com>
2022-07-03 03:02:50 +0000

    They said only 4 files so I deleted some

--- calculate_alpha_length.py
+++
@@ -1,2 +0,0 @@
-def cal_alpha_length_of_str(str_):
-    return len([i for i in str_ if not i=="\n" and not i==" "])
--- exam_if_str_in.py
+++
@@ -1,4 +0,0 @@
-def exam_if_str_this_in(str_, test_str):
-    if test_str.lower() in str_.lower():
-        return True
-    return False
