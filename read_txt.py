fd166dbd352f5281adcd6b5cf9e7dccf8946d545
chenyangkang <chenyangkang24@outlook.com>
2022-07-03 02:42:03 +0000

    add try something

--- read_txt.py
+++ read_txt.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 file = open('hello.txt').read()
 print('------------------')
+print('OK! Let\'s try some thing!')
+
+
+print('------------------')
 print(file)
 print('------------------')
 
