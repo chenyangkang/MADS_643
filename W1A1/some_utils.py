b7ba68b6f18fc6d61523081129cba419a9ed9f71
chenyangakang <chenyangkang24@outlook.com>
2022-07-03 05:22:51 +0000

    add

--- W1A1/some_utils.py
+++
@@ -1,7 +0,0 @@
-def cal_alpha_length_of_str(str_):
-    return len([i for i in str_ if not i=="\n" and not i==" "])
-
-def exam_if_str_this_in(str_, test_str):
-    if test_str.lower() in str_.lower():
-        return True
-    return False
--- W1A2/rodeo.py
+++
@@ -1,7 +0,0 @@
-def rename_rodeo_columns(columns):
-    out_list=[]
-    for name in columns:
-        new_name = name.lower().replace(' ','_').strip('data')
-        out_list.append(new_name)
-    
-    return out_list
